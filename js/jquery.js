191d6fc9bc5ab1e692b5910fd6b6ef1ce22c74af
Sarthak Duggal <52778617+sardug10@users.noreply.github.com>
2020-08-02 02:24:08 +0530

    Add files via upload

---
+++ js/jquery.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  $(".trending").waypoint(
+    function (direction) {
+      if (direction == "down") {
+        $("nav").addClass("sticky");
+      } else {
+        $("nav").removeClass("sticky");
+      }
+    },
+    {
+      offset: "60px",
+    }
+  );
+});
---
+++ js/script.js
@@ -0,0 +1,90 @@
+// JAVASCRIPT FOR TYPING-EFFECT
+const afterTexts = [
+  "Find your dream bridal trousseau",
+  "Makeover your custom closet",
+  "Unleash wide range of clothing desings",
+];
+
+words = "";
+letters = "";
+
+index = 0;
+count = 0;
+
+(function type() {
+  if (index === afterTexts.length) {
+    index = 0;
+  }
+  words = afterTexts[index];
+  letters = words.slice(0, ++count);
+  document.querySelector(".hero__tagline").textContent = letters;
+  if (letters.length === words.length) {
+    index++;
+    count = 0;
+  }
+  setTimeout(type, 300);
+})();
+
+//JAVASCRIPT FOR CAROUSEL
+
+var elem = document.querySelector(".main-carousel");
+var flkty = new Flickity(elem, {
+  cellAlign: "left",
+  containe: true,
+  autoPlay: 1500,
+  pageDots: false,
+  wrapAround: true,
+  pauseAutoPlayOnHover: true,
+  imagesLoaded: true,
+  prevNextButtons: false,
+});
+
+elem.addEventListener("mouseleave", () => {
+  flkty.playPlayer();
+});
+
+// JAVASCRIPT FOR MAPS
+
+mapboxgl.accessToken =
+  "pk.eyJ1Ijoic2FydGhhazEwZCIsImEiOiJja2RieDhxbXYwd2FxMnByeHBxOWtqemZwIn0.rc5k_5BeD33xqdgomIhv-g";
+var map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/sarthak10d/ckdbxok2p07su1iqv7vpov4bj",
+  scrollZoom: false,
+  center: [77.174849, 28.689287],
+  zoom: 15,
+});
+
+const loc = "Ashok Vihar,Delhi,India 110052";
+const el = document.createElement("div");
+el.className = "marker";
+
+const bounds = new mapboxgl.LngLatBounds();
+
+// Add marker
+new mapboxgl.Marker({
+  element: el,
+  anchor: "bottom",
+})
+  .setLngLat([77.174849, 28.689287])
+  .addTo(map);
+
+// Add popup
+new mapboxgl.Popup({
+  offset: 30,
+})
+  .setLngLat([77.174849, 28.689287])
+  .setHTML(`<p>${loc}</p>`)
+  .addTo(map);
+
+// Extend map bounds to include current location
+bounds.extend([77.174849, 28.689287]);
+
+map.fitBounds(bounds, {
+  padding: {
+    top: 200,
+    bottom: 150,
+    left: 100,
+    right: 100,
+  },
+});
